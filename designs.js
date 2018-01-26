d72c5bac0f657f763d74d599f1f7b8821ff125fd
Anwana Ntofon <anwanantofon@gmail.com>
2018-01-26 15:24:48 -0800

    Modified width ID in HTML, assigned jQuery selected IDs to vars

--- designs.js
+++ designs.js
@@ -1,10 +1,16 @@
 // Select color input
+var colorPicker = $("#colorPicker");
 // Select size input
+var ht = $("#inputHeight");
+var wt = $("#inputWeight");
+/* 1. For instance, the submit button, the table, and the color picker need to be accessed. The value of the color selected needs to be stored as well, since the clicked cell in the table needs to be set to the selected color.
+2. Add event listeners to the relevant DOM elements, so that user input can be color values and table sizes can be dynamically set by the user.
+3. Set the size of the cross stitch canvas as an N by M grid with the makeGrid() function. Use your knowledge of JavaScript loops to dynamically clear and create the table based on user input. Each cell should have an event listener that sets the background color of the cell to the selected color. */
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
 
-// Your code goes here!
+  // Your code goes here!
 
 }
