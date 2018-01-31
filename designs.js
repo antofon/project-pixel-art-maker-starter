166c197746ad1f20fb008135d8a3a03607d9d542
Anwana Ntofon <anwanantofon@gmail.com>
2018-01-30 20:52:25 -0800

    Can now fill cells with default and changed color with user clicks

--- designs.js
+++ designs.js
@@ -3,31 +3,46 @@ var colorPicker = $("#colorPicker");
 // Select size input for table
 var htVal = 0;
 var wtVal = 0;
+//select Color
+var color = null;
 // Select submit button
 var submit = $(":submit");
 //select canvas
 var pixelCanvas = $("#pixelCanvas");
-/* 1. For instance, the submit button, the table, and the color picker need to be accessed. The value of the color selected needs to be stored as well, since the clicked cell in the table needs to be set to the selected color.
-2. Add event listeners to the relevant DOM elements, so that user input can be color values and table sizes can be dynamically set by the user.
-3. Set the size of the cross stitch canvas as an N by M grid with the makeGrid() function. Use your knowledge of JavaScript loops to dynamically clear and create the table based on user input. Each cell should have an event listener that sets the background color of the cell to the selected color. */
 
 function makeGrid(htVal, wtVal) {
-  //use index var to create unique IDs for each row
+  //use index var to create unique IDs for each row and cell (cell1 in row 0 and cell1 in row1 make up col1)
   //this ensures that when columns (<td></td>) are appended, they will be appended to unique IDs, not all rows (<tr></tr>)
-  var index = 0;
+  var rowIndex = 0;
+  var cellIndex = 0;
   for (var i = 0; i < htVal; i++) {
-    pixelCanvas.append("<tr id='row" + index + "'></tr>");
+    pixelCanvas.append("<tr id='row" + rowIndex + "'></tr>");
     for (var j = 0; j < wtVal; j++) {
-      $("#row" + index).append('<td></td>');
+      $("#row" + rowIndex).append("<td id='cell" + cellIndex + "'></td>");
+      /*when a cell is clicked, store the current value of the colorPicker
+      and use .css() to change the backgroundColor of the current cell using $(this)*/
+      $("#cell" + cellIndex).click(function(event) {
+        color = $("#colorPicker").val();
+        $(this).css("backgroundColor", color);
+      }).hover().css("cursor", "pointer");
+      //to create unique ID for the cell
+      cellIndex++;
     }
-    index++;
+    //to create unique ID for the row
+    rowIndex++;
   }
 }
-// When size is submitted by the user, call makeGrid()
-submit.on("click", function(event) {
+
+//when the colorPicker is changed, store the new color in global var
+$("#colorPicker").on("change", function() {
+  color = $(this).val();
+});
+
+submit.click(function(event) {
   //clear Canvas after each user submission
   $("#pixelCanvas").html("");
   //capture values for height and width of canvas
+  color = $("#colorPicker").val();
   htVal = $("#inputHeight").val();
   wtVal = $("#inputWidth").val();
   makeGrid(htVal, wtVal);
